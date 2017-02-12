bd6430a0dc00e35ccd4a8d2f3fc058961bfdfa67
Nishant Sinha <nishnha@gmail.com>
2017-02-12 00:07:57 -0500

    items load from s3 server

--- js/products.js
+++ js/products.js
@@ -1,35 +1,33 @@
+var items = []
+var container = document.getElementById("item-container");
+
 function template(item) {
     return `
         <div class="item" id=${item.name}>
-            <img src="res/${item.image}" class="item-image">
+            <img src="http://s3.amazonaws.com/com.nishnha.webdev/${item.image}" class="item-image">
             <h3>${item.name}</h3>
-            <p class="item-text">${item.description}</p>
+            <ul class="item-text">${item.description}</ul>
         </div>
     `
 };
 
-let silvertone = {
-    name: 'Bulova Silvertone',
-    image: 'bulova_silvertone.jpeg',
-    description: `
-	Stainless Steel
-    	Made in USA or Imported
-        Ultra-precise round watch with blue sunray dial, three chronograph sub dials, luminous hands and markers, screw-back, and tachymeter
-        43 mm stainless steel case with mineral dial window
-        Quartz movement with analog display
-        Stainless steel band with double-press deployant closure
-        Water resistant to 100 m (330 ft): In general, suitable for swimming and snorkeling, but not scuba diving
-    `
+function createItems(responseText) {
+    for (let i = 0; i < responseText.length; i++) {
+       items.push(template(responseText[i]));
+    }
+    for (let i = 0; i < items.length; i++) {
+        container.insertAdjacentHTML('beforeend', items[i]);
+    }
+};
+
+var xhttp = new XMLHttpRequest();
+xhttp.open("GET", "https://s3.amazonaws.com/com.nishnha.webdev/products.json", true);
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        createItems(JSON.parse(xhttp.responseText));
+    }
 }
-
-var items = [];
-
-items.push(template(silvertone));
-
-var container = document.getElementById("item-container");
-
-for (let i = 0; i < items.length; i++) {
-    container.insertAdjacentHTML('beforeend', items[i]);
+xhttp.onerror = function() {
+    console.log("Connection failed");
 }
-
-
+xhttp.send();
